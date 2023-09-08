5a99ec3153607ed6177b16ab8e6bb2eabe3ee6ed
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2023-09-08 01:39:09 +0100

    new commit

--- utils/redis.js
+++ utils/redis.js
@@ -1,16 +1,16 @@
 import { createClient } from 'redis';
 
 class RedisClient {
-    constructor(){
-        this.connected = false
-        this.client = createClient()
-        this.client.on('error', err=>console.log('Redis Client Error', err),
-        this.connected = false
-        )
-        this.client.on('connect', ()=>{
-            this.connected = true
-        })
-    }
+  constructor(){
+    this.connected = false
+    this.client = createClient()
+    this.client.on('error', err=>console.log('Redis Client Error', err),
+    this.connected = false
+    )
+    this.client.on('connect', ()=>{
+        this.connected = true
+    })
+}
    isAlive() {
         return this.connected
     }
